e0083233cb353f95fda930d7b6cf67218eab650d
Arne Johannsmann <arnej@google.com>
2023-12-02 20:35:27 +0000

    Day 2
    Change-Id: Iaa1fa2f7bd99261f94bd14c1f1c75e2608febc6a

---
+++ day2/src/main.rs
@@ -0,0 +1,104 @@
+fn main() {
+    println!("Hello, world!");
+    let given = Cubes {
+        r: 12,
+        g: 13,
+        b: 14,
+    };
+    println!(
+        "SILVER: The example solution is : {}",
+        day1("test1.txt", given)
+    );
+    println!("SILVER: The solution is : {}", day1("input.txt", given));
+    println!(
+        "GOLD: The example solution is : {}",
+        day2("test1.txt", given)
+    );
+    println!("GOLD: The solution is : {}", day2("input.txt", given));
+}
+
+#[derive(Clone, Copy)]
+struct Cubes {
+    r: u64,
+    g: u64,
+    b: u64,
+}
+
+impl Cubes {
+    fn max(self, other: Cubes) -> Cubes {
+        Cubes {
+            r: self.r.max(other.r),
+            g: self.g.max(other.g),
+            b: self.b.max(other.b),
+        }
+    }
+}
+
+impl From<&str> for Cubes {
+    fn from(s: &str) -> Self {
+        let mut cube = Cubes { r: 0, b: 0, g: 0 };
+        let cubes = s
+            .split(',')
+            .map(|cube| cube.trim().split_once(' ').unwrap())
+            .map(|(count, color)| (count.parse().unwrap(), color));
+        for (count, color) in cubes {
+            match color {
+                "red" => cube.r = count,
+                "blue" => cube.b = count,
+                "green" => cube.g = count,
+                _ => {}
+            }
+        }
+        cube
+    }
+}
+
+struct Game {
+    id: u64,
+    rounds: Vec<Cubes>,
+}
+
+impl Game {
+    fn get_cubes_needed_to_play(&self) -> Cubes {
+        self.rounds
+            .iter()
+            .fold(Cubes { r: 0, b: 0, g: 0 }, |acc, &now| acc.max(now))
+    }
+}
+
+fn read(loc: &str) -> Vec<Game> {
+    std::fs::read_to_string(loc)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.split_once("Game ")
+                .unwrap()
+                .1
+                .split_once(": ")
+                .unwrap()
+        })
+        .map(|(id, games)| Game {
+            id: id.parse().unwrap(),
+            rounds: games.split(';').map(|cubes| cubes.into()).collect(),
+        })
+        .collect()
+}
+
+fn day1(loc: &str, given: Cubes) -> u64 {
+    let Cubes { r, b, g } = given;
+    read(loc)
+        .into_iter()
+        .map(|game| (game.id, game.get_cubes_needed_to_play()))
+        .filter(|(_, c)| c.r <= r && c.b <= b && c.g <= g)
+        .map(|(id, _)| id)
+        .sum()
+}
+
+fn day2(loc: &str, given: Cubes) -> u64 {
+    let Cubes { r, b, g } = given;
+    read(loc)
+        .into_iter()
+        .map(|game| game.get_cubes_needed_to_play())
+        .map(|c| c.r * c.g * c.b)
+        .sum()
+}
