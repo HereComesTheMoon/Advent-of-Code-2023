aeadfe6e18f3c0cce3610c709da3ee07b4e3d6b8
Arne Johannsmann <arnej@google.com>
2023-12-04 12:05:08 +0000

    Day4
    Change-Id: I909810b810c9cf819af3404883d216fbed84bf2c

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,14 +1,16 @@
-use std::{fs::read_to_string, collections::HashSet};
+use std::{collections::HashSet, fs::read_to_string};
 
 fn main() {
     println!("Hello, world!");
     println!("SILVER: The example solution is : {}", day1("example.txt"));
     println!("SILVER: The solution is : {}", day1("input.txt"));
-    // println!("GOLD: The example solution is : {}", day2("test.txt"));
-    // println!("GOLD: The solution is : {}", day2("input.txt"));
+    println!("GOLD: The example solution is : {}", day2("example.txt"));
+    println!("GOLD: The solution is : {}", day2("input.txt"));
 }
 
-fn read(loc: &str) -> Vec<(HashSet<u32>, HashSet<u32>)> {
+type Round = (HashSet<u32>, HashSet<u32>);
+
+fn read(loc: &str) -> Vec<Round> {
     read_to_string(loc)
         .unwrap()
         .lines()
@@ -28,13 +30,32 @@ fn read(loc: &str) -> Vec<(HashSet<u32>, HashSet<u32>)> {
 }
 
 fn day1(loc: &str) -> usize {
-    let data = read(loc);
-    let mut res = 0;
-    for (win, have) in data.iter() {
-        let count = win.intersection(have).count() as u32;
-        if 1 <= count {
-            res += usize::pow(2_usize, count - 1);
+    read(loc)
+        .iter()
+        .map(|(win, have)| {
+            let count = win.intersection(have).count() as u32;
+            if 1 <= count {
+                usize::pow(2, count - 1)
+            } else {
+                0
+            }
+        })
+        .sum()
+}
+
+fn day2(loc: &str) -> usize {
+    let rounds = read(loc);
+    let mut cards = vec![1; rounds.len()];
+
+    for (k, round) in rounds.into_iter().enumerate() {
+        let (win, have) = round;
+        let victories = win.intersection(&have).count();
+        for i in 1..=victories {
+            if cards.len() <= k + i {
+                break;
+            }
+            cards[k + i] += cards[k];
         }
     }
-    res
+    cards.into_iter().sum()
 }
