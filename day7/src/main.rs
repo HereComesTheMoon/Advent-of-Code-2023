2ee66ab43b236d45c5db4174c05381c54a80c64e
Arne Johannsmann <arnej@google.com>
2023-12-07 15:11:11 +0000

    Day 7 done. Worst code
    Change-Id: Id1c1526a14fd6d7f4395165405d41543a05d6b6e

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,17 +1,14 @@
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::mem::discriminant;
 
 fn main() {
     println!("Hello, world!");
 
-    use Label::*;
-    let mut v = [_5, _3, _9, _7, A, J, _4];
-    v.sort_unstable();
-    println!("{:?}", v);
     println!("SILVER: The example result is : {}", silver("example.txt"));
     println!("SILVER: The result is : {}", silver("input.txt"));
-    // println!("GOLD: The example result is : {}", gold("example.txt"));
-    // println!("GOLD: The result is : {}", gold("input.txt"));
+    println!("GOLD: The example result is : {}", gold("example.txt"));
+    println!("GOLD: The result is : {}", gold("input.txt"));
 }
 
 #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Debug)]
@@ -29,6 +26,15 @@ enum Label {
     Q,
     K,
     A,
+}
+
+impl Label {
+    fn is_j(&self) -> bool {
+        match self {
+            Label::J => true,
+            _ => false,
+        }
+    }
 }
 
 impl From<char> for Label {
@@ -87,6 +93,50 @@ impl Hand {
             _ => panic!("Oh no!"),
         }
     }
+
+    fn get_joker_type(&self) -> Type {
+        let mut counter = HashMap::new();
+        for card in self.cards.iter().filter(|card| !card.is_j()) {
+            let d = discriminant(card);
+            counter.insert(d, 1 + *counter.get(&d).unwrap_or(&0));
+        }
+        let num_j = 5 - counter.values().sum::<isize>();
+        match (counter.len(), num_j) {
+            (_, 0) => self.get_type(),
+            (2, 3) => Type::Four,
+            (_, 3 | 4 | 5) => Type::Five,
+            (1, _) => Type::Five,
+            (2, 2) => Type::Four,
+            (3, 2) => Type::Three,
+            (4, 1) => Type::OnePair,
+            (3, 1) => Type::Three,
+            (2, 1) if *counter.values().max().unwrap() == 3 => Type::Four,
+            (2, 1) => Type::FullHouse,
+            _ => todo!(),
+        }
+    }
+
+    fn joker_cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self
+            .get_joker_type()
+            .partial_cmp(&other.get_joker_type())
+            .unwrap()
+        {
+            std::cmp::Ordering::Equal => {
+                for (a, b) in self.cards.iter().zip(other.cards.iter()) {
+                    return match (a.is_j(), b.is_j()) {
+                        (true, true) => continue,
+                        (true, false) => Ordering::Less, // TODO
+                        (false, true) => Ordering::Greater,
+                        (false, false) if a == b => continue,
+                        (false, false) => a.cmp(b),
+                    };
+                }
+                panic!("Oh this really should not happen")
+            }
+            val => val,
+        }
+    }
 }
 
 impl PartialEq for Hand {
@@ -130,4 +180,53 @@ fn silver(loc: &str) -> u64 {
         .enumerate()
         .map(|(rank, bid)| (1 + rank) as u64 * bid)
         .sum()
+}
+
+fn gold(loc: &str) -> u64 {
+    let mut hands = read(loc);
+    hands.sort_unstable_by(|s, other| {
+        match s
+            .get_joker_type()
+            .partial_cmp(&other.get_joker_type())
+            .unwrap()
+        {
+            std::cmp::Ordering::Equal => s.joker_cmp(other),
+            val => val,
+        }
+    });
+    hands
+        .into_iter()
+        .map(|hand| hand.bid)
+        .enumerate()
+        .map(|(rank, bid)| (1 + rank) as u64 * bid)
+        .sum()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn gold_ex() {
+        assert_eq!(gold("example.txt"), 5905);
+        let res = gold("input.txt");
+        assert!(249577022 < res);
+        assert!(249724623 < res);
+    }
+
+    #[test]
+    fn order_joker() {
+        use Label::*;
+        let a = Hand {
+            cards: [A, A, A, A, A],
+            bid: 0,
+        };
+        let b = Hand {
+            cards: [J, A, A, A, A],
+            bid: 0,
+        };
+
+        let ord = a.joker_cmp(&b);
+        assert!(matches!(ord, Ordering::Greater));
+    }
 }
