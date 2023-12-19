a1dc5ab25390a77c13efcaddb107fe17677f4cb4
Mond <herecomesthemoon@protonmail.com>
2023-12-19 22:37:31 +0100

    Day 19 b

--- day19/main.py
+++ day19/main.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from typing import List
+import copy
 
 
 @dataclass
@@ -16,6 +17,38 @@ class Step:
             case '>':
                 return part[self.field] > self.value
 
+    def split(self, part):
+        a, b = part[self.field]
+        res = []
+        match self.op:
+            case '<' if b < self.value:
+                res.append((self.goal, part))
+            case '<' if self.value <= a:
+                res.append((None, part))
+            case '>' if a > self.value:
+                res.append((self.goal, part))
+            case '>' if self.value >= b:
+                res.append((None, part))
+            case '<':
+                c = self.value
+                aa = copy.copy(part)
+                aa[self.field] = (a, c)
+                res.append((self.goal, aa))
+                bb = copy.copy(part)
+                bb[self.field] = (c, b)
+                res.append((None, bb))
+            case '>':
+                c = self.value
+                aa = copy.copy(part)
+                aa[self.field] = (a, c + 1)
+                res.append((None, aa))
+                bb = copy.copy(part)
+                bb[self.field] = (c + 1, b)
+                res.append((self.goal, bb))
+            case _:
+                print("what?")
+        return res
+
 
 @dataclass
 class Workflow:
@@ -28,6 +61,23 @@ class Workflow:
             if step.check(part):
                 return step.goal
         return self.last
+
+    def split(self, part):
+        done = []
+        parts = [part]
+        for step in self.steps:
+            doing = []
+            while parts:
+                part = parts.pop()
+                doing.extend(step.split(part))
+            for next, part in doing:
+                if next is None:
+                    parts.append(part)
+                else:
+                    done.append((next, part))
+        for part in parts:
+            done.append((self.last, part))
+        return done
 
 
 def read(loc):
@@ -65,7 +115,6 @@ def parse_workflow(line):
         
 def parse_part(line):
     chunks = line[1:len(line)-1].split(",")
-    assert len(chunks) == 4
     d = {}
     for chunk in chunks:
         d[chunk[0]] = int(chunk[2:])
@@ -86,6 +135,23 @@ class Sorter:
             next = self.wfs[now].sort(part)
         return next == 'A'
 
+    def rec(self, part, wf):
+        parts = self.wfs[wf].split(part)
+        res = 0
+        for wf, part in parts:
+            if any(b <= a for a, b in part.values()):
+                return 0
+            if wf == 'A':
+                val = 1
+                for (a, b) in part.values():
+                    val *= (b - a)
+                res += val
+                continue
+            if wf == 'R':
+                continue
+            res += self.rec(part, wf)
+        return res
+        
 
 def silver(loc):
     wfs, parts = read(loc)
@@ -97,7 +163,20 @@ def silver(loc):
     return res
 
 
+def gold(loc):
+    wfs, _ = read(loc)
+    start = {
+        'x': (1, 4001),
+        'm': (1, 4001),
+        'a': (1, 4001),
+        's': (1, 4001)
+    }
+    sorter = Sorter(wfs)
+    return sorter.rec(start, "in")
+
+
 if __name__ == '__main__':
     print(silver("test.txt"))
     print(silver("input.txt"))
-        
+    print(gold("test.txt"))
+    print(gold("input.txt"))
